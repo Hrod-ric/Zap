353640bdc0b53a6fce47a42153d1731380d00c48
Rodrigo-Bastos <rodrigo.bastos047+github@gmail.com>
2022-11-05 13:58:42 -0300

    atualização

--- comandos.js
+++ comandos.js
@@ -3,8 +3,8 @@ const listar = (pasta)=>{
     var Lista = fs.readdirSync("./Dados/"+pasta).map(p=>JSON.parse(fs.readFileSync("./Dados/"+pasta+"/"+p)));
     return Lista;
 }
-Array.prototype.adicionarSubComandos = function(nome, func){
-    this.push({nome, func})
+Array.prototype.adicionarSubComandos = function(nome, desc,func){
+    this.push({nome, desc, func})
     return this
 }
 const contato = async (msg)=>{
@@ -35,7 +35,7 @@ var comandos = {
         texto = msg.body.toLowerCase().split(' ')[2]
 
         var lista = []
-        lista.adicionarSubComandos("pes",async()=>{
+        lista.adicionarSubComandos("pes","mo",async()=>{
             if(texto){
                 var String= "População do "+texto+"\n- " + listar(texto).map(p=>p.nome).join("\n- ");
                 await bot.sendMessage(numero, String)
@@ -50,7 +50,7 @@ var comandos = {
             msg.reply("Enviado!")
         })
 
-        lista.adicionarSubComandos("fei",async()=>{
+        lista.adicionarSubComandos("fei","my",async()=>{
             if(texto){
                 var String= "Feiticos de "+texto+"\n- " + listar(texto).map(p=>p.nome).join("\n- ");
                 await bot.sendMessage(numero, String)
@@ -63,7 +63,7 @@ var comandos = {
             msg.reply("Enviado!")
         })
         
-        lista.adicionarSubComandos("arq",async()=>{
+        lista.adicionarSubComandos("arq","mu",async()=>{
             if(texto){
                 var String= "Lista de Arquivos\n"
                             +texto+"\n- " + fs.readdirSync("./Dados/"+texto).map(p=>p).join("\n- ")
@@ -73,19 +73,20 @@ var comandos = {
             }
 
             var String =    "Lista de Arquivos"+
-                            "\n\nImagens\n- " + fs.readdirSync("./Dados/Imagens").map(p=>p).join("\n- ") +
-                            "\n\nAudios\n- " + fs.readdirSync("./Dados/Audios").map(p=>p).join("\n- ") +
-                            "\n\nVideos\n- " + fs.readdirSync("./Dados/Videos").map(p=>p).join("\n- ")
+                            "\n\nImagens\n- " + fs.readdirSync("./Dados/Imagens").join("\n- ") +
+                            "\n\nAudios\n- " + fs.readdirSync("./Dados/Audios").join("\n- ") +
+                            "\n\nVideos\n- " + fs.readdirSync("./Dados/Videos").join("\n- ")
 
             await bot.sendMessage(numero, String)
             msg.reply("Enviado!")
         })
 
-        var comandoSelecionado= lista.find(e=>comando.startsWith(e.nome))
+        var comandoSelecionado= lista.find(e=>comando.startsWith(e.nome)).func
         if(comandoSelecionado) {
             try{
                 comandoSelecionado()
-            }catch{
+            }catch (e){
+                console.log(e);
                 msg.reply("Ocorreu um erro!")
             }
         }else{
@@ -98,7 +99,6 @@ var comandos = {
         var numero = (await contato(msg)).numero
         var chat = (await contato(msg)).chat
         var encontrou = Players.find(p=>p.contato == numero.replace("@c.us",""));
-        //console.log(chat.id.user);
         if(encontrou){
             await bot.sendMessage(numero, 
                 `Nome: ${encontrou.nome} \n`+
@@ -112,11 +112,6 @@ var comandos = {
 
     "consultar" :  async (msg, bot, whatsapp)=>{
         var Lista=listar("Players").concat(listar("Povoado"),listar("Rollenspiel"),listar("Feiticos"));
-        //var texto = msg.body.toLowerCase().slice(11);
-        /*var comando = msg.body.toLowerCase().split(' ')[0]
-        var indice = msg.body.indexOf(comando) + comando.length;
-        var texto = msg.body.toLowerCase().slice(indice+1, msg.body.length);*/
-        //console.log(message);
         var texto = msg.body.toLowerCase().split(' ').filter((_,i)=>i).join(" ")
         var encontrou = Lista.find(p=>p.id == texto);
 
@@ -174,14 +169,15 @@ var comandos = {
         }catch{
             await bot.sendMessage(msg.from,"Não encontrado")
         }
+        
     },
 
-    "env, envi, envia, enviar" : async (msg, bot, whatsapp)=>{
+    "enviar" : async (msg, bot, whatsapp)=>{
         var numero = msg.body.split(' ')[1];
             numero = numero.startsWith('@') ? numero.replace("@","") : numero
             numero = numero.includes('@g.us') ? numero :
             numero = numero.includes('@c.us') ? numero : `${numero}@c.us`;
-        //console.log(numero);
+
         var texto = msg.body.split(' ')[2];
         try{
             if(texto.includes(".mp4")){
@@ -197,16 +193,19 @@ var comandos = {
     }
 
 }
-//comandos["env"] = comandos["envi"] = comandos["envia"] = comandos["enviar"]
+/*comandos["env"] = comandos["envi"] = comandos["envia"] = comandos["enviar"]
 comandos["tocar"] = async (msg, bot, whatsapp)=>{
     msg.reply("toquei")
-}
+}*/
 
+/*lista.adicionarSubComandos("com","",async()=>{
+    msg.reply(lista.map(e=>e.nome + " - " + e.desc).join("\n"))
+})*/
 
-comandos["comandos2"] = async (msg, bot, whatsapp)=>{
+/*comandos["comandos2"] = async (msg, bot, whatsapp)=>{
     var lista = Object.keys(comandos)
     msg.reply(lista.join("\n"))
-}
+}*/
 
 
 module.exports = comandos
